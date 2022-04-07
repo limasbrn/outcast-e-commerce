c2e81c2d9d03b49a9a18576f9ed60a8aa5ec84ae
Sabrina Silva <lima.sbrn@gmail.com>
2022-04-07 17:31:15 -0300

    atualização carrinho

--- src/components/btn-gray.js
+++ src/components/btn-gray.js
@@ -3,7 +3,7 @@ import React from 'react' ;
 function GrayBtn(props){
     return(
         <React.Fragment>
-            <button className="flex w-[280px] h-[35px] text-white text-[12px] tracking-[5px] bg-[#868686] rounded-[5px] items-center justify-center">{props.name}</button>
+            <button className="flex w-[280px] h-[35px] text-white text-[12px] tracking-[5px] bg-[#868686] rounded-[5px] items-center justify-center mb-[15px]">{props.name}</button>
         </React.Fragment>
         
     )
--- src/pages/carrinho.js
+++ src/pages/carrinho.js
@@ -10,18 +10,18 @@ function CarrinhoPage() {
         <section>
                <HomeLink page='Sacola'/>
                <PageTitle h1='SACOLA'/> 
-               <div className="flex w-full h-[280px] justify-center">
+               <div className="flex w-full h-[300px] justify-center">
                    <div className="flex w-4/5 h-full bg-red-400 justify-center">
                        <div className="flex flex-col justify-between items-center w-2/6 h-full bg-gray-200">
                            <div className="flex w-5/6 h-[135px] bg-red-100"></div>
                            <div className="flex w-5/6 h-[135px] bg-red-200"></div>
                        </div>
                        <div className="flex w-2/6 h-full rounded-[5px] justify-center items-center bg-gray-100">
-                           <div className="flex flex-col w-5/6 h-5/6 bg-red-300">
-                               <h1 className="font-semibold">TOTAL:</h1>
-                               <h1 className="font-semibold">R$ 535,00</h1>
-                               <span className="text-[#868686] text-[14px]">Esta compra é elegível as regras de FRETE GRÁTIS</span>
-                               <div>
+                           <div className="flex flex-col w-5/6 h-5/6 justify-center bg-red-300">
+                               <h1 className="font-semibold mb-[10px]">TOTAL:</h1>
+                               <h1 className="font-semibold mb-[15px]">R$ 535,00</h1>
+                               <span className="w-[80%] text-center self-center text-[#868686] text-[12px] mb-[15px]">Esta compra é elegível as regras de <strong>FRETE GRÁTIS</strong></span>
+                               <div className="flex flex-col w-full h-auto items-center justify-around">
                                     <GrayBtn name="CONTINUAR COMPRANDO"/>
                                     <BlackBtn name="FINALIZAR COMPRAS"/>
                                </div>
